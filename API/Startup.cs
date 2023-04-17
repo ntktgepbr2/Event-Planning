aef4612fa3498465ea6f5ec04a5bd4edfb9a5601
Daniil Krishtofik <d.krishtofik@itransition.com>
2023-04-17 13:53:47 +0300

    Add delete and update activity endpoint.

--- API/Startup.cs
+++ API/Startup.cs
@@ -13,6 +13,10 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Persistence;
+using Application.Activities;
+using MediatR;
+using Application.Mappers;
+using API.Extensions;
 
 namespace API
 {
@@ -28,21 +32,8 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
-            });
-            services.AddDbContext<DataContext>(opt =>{
-                opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
-            }
-            );
-            services.AddCors(opt => {
-                opt.AddPolicy("CorsPolicy", policy => {
-                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
-                });
-            });
+            services.AddApplicationServices(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
