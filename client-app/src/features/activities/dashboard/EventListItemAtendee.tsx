f22577eeab1d68d5b4e9fbac2eff4d1b21c7ce70
Daniil Krishtofik <d.krishtofik@itransition.com>
2023-05-11 17:09:22 +0300

    Add profile popup

--- client-app/src/features/activities/dashboard/EventListItemAtendee.tsx
+++ client-app/src/features/activities/dashboard/EventListItemAtendee.tsx
@@ -1,6 +1,7 @@
 import { observer } from "mobx-react-lite";
-import { List, Image } from "semantic-ui-react";
+import { List, Image, Popup } from "semantic-ui-react";
 import { Profile } from "../../../app/models/profile";
+import ProfileCard from "../../profiles/ProfileCard";
 
 interface Props {
   attendees: Profile[];
@@ -10,9 +11,19 @@ export default observer(function EventListItemAtendee({ attendees }: Props) {
   return (
     <List horizontal>
       {attendees.map((attendee) => (
-        <List.Item key={attendee.userName}>
-          <Image size='mini' circular src={attendee.image || "/assets/user.png"} />
-        </List.Item>
+        <Popup
+          hoverable
+          key={attendee.userName}
+          trigger={
+            <List.Item key={attendee.userName}>
+              <Image size='mini' circular src={attendee.image || "/assets/user.png"} />
+            </List.Item>
+          }
+        >
+          <Popup.Content>
+            <ProfileCard profile={attendee} />
+          </Popup.Content>
+        </Popup>
       ))}
     </List>
   );
